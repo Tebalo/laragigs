fe6f7cb99405e5a1c2e6aa18cd0d39ce88b99b40
Tebalo <btebalo@gmail.com>
2023-02-12 02:26:00 +0200

    Initial commit

---
+++ app/Models/listing.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+class Listing
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Here is where you can register web routes for your application. These
+                 routes are loaded by the RouteServiceProvider within a group which
+|                contains the "web" middleware group. Now create something great!'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Here is where you can register web routes for your application. These
+                 routes are loaded by the RouteServiceProvider within a group which
+|                contains the "web" middleware group. Now create something great!'
+            ]
+        ];
+    }
+    public static function find($id)
+    {
+        $listings = self::all();
+
+        foreach ($listings as $listing) {
+            if ($listing[$id] == $id) {
+                return $listing;
+            }
+        }
+    }
+}
