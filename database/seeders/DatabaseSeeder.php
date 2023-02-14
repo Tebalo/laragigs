f3706177534de530fb8de1d39e1e48c9e2bafcbc
Tebalo <btebalo@gmail.com>
2023-02-15 00:32:05 +0200

    Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //\App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Listing::create([
+            'title' => 'Laravel Senior Developer',
+            'tags' => 'laravel, javascript',
+            'location' => 'Acme Corp',
+            'email' => 'email@email.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur 
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+            consequat. Duis aute irure dolor in reprehenderit in voluptate 
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+            sint occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum'
+        ]);
+
+        Listing::create([
+            'title' => 'Full-Stack Engineer',
+            'tags' => 'laravel, backend, api',
+            'company' => 'Stark Industries',
+            'location' => 'New York, NY',
+            'email' => 'email2@email.com',
+            'website' => 'https://www.starkindustries.com',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur 
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+            consequat. Duis aute irure dolor in reprehenderit in voluptate 
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+            sint occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum'
+        ]);
     }
 }
